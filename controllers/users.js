bd9464a2451a87ad03361373f9928d3a1643e864
mercyikpe <ikpemercy1@gmail.com>
2023-04-15 02:40:57 +0200

    add user data validation in user controller

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,36 @@ const registerUser = async (req, res) => {
   try {
     const { name, email, phone, password } = req.fields;
     const { image } = req.files;
+
+    // check that fields are not empty
+    if(!name || !email || !phone || !password) {
+        return res.status(404).json({
+            message: "name, email phone or password is missing",
+        });
+    }
+
+    // check password length
+    if(password.length < 6) {
+        return res.status(404).json({
+            message: "Minimum length for password is 6",
+        });
+    }
+
+    // if image is added, check image size
+    if(image && image.size > 1000000) {
+        return res.status(400).json({
+            message: "maximum size of image is 1MB",
+        });
+    }
+
+    // check if user already exist by id
+     const isExist = User.findOne({email: email})
+    if(isExist) {
+        res.status(400).json({
+            message: "User with this email already exist"
+        })
+    }
+
     res.status(201).json({
       message: "user created",
     });
